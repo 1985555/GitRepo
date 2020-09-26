0a881fb5f8a1e64d5067dad927c12685c7439d86
saeed <s.chebli@hotmail.com>
2020-09-26 11:18:46 +0300

    add pagination

--- src/App.js
+++ src/App.js
@@ -1,50 +1,74 @@
-import React, { Component } from 'react';
-
+import React, { useState, useEffect } from 'react';
 import Contacts from './Components/Contacts';
 import User from './Components/User';
-
-//import logo from './logo.svg';
 import './App.css';
+import Pagination from './Components/pagination';
+//import axios from 'axios';
 
 // A Function component also returns HTML, and behaves pretty much the same way as a Class component, 
 //but Class components have some additions, and will be preferred in this tutorial. 
 // example function Car () ... end then .render(<Car/>, getelementby...)
 //function App() {
-
-class App extends Component{  //the component's name must start with an upper case letter.
+const App = () => { 
+//class App extends Component{  //the component's name must start with an upper case letter.
    // Every component also requires a render() method, this method returns HTML.
-constructor(props){
-  super();
-  this.state = { 
-    items:[], 
-    isLoaded: false,
-  };
+const [count, increment ] = useState(0); // number 
+
+// Pagination variables 
+const [itemperpage] = useState(4);
+const [isLoaded, setIsLoaded ] = useState(false);
+const [currentPage, setCurrenPage ] = useState(1);
+const [items, updateItems ] = useState([]);
+
+// constructor(props){
+//   super();
+//   this.state = { 
+//     items:[], 
+//     isLoaded: false,
+//   };
   // All React components must act like pure functions(c.a.d must never modify its own props) 
   // with respect to their props.
   // In React, component properties should be kept in an object called state.
-}   
+//}   
 
 // the component did mount the method runs after the render method, 
 // then updates the render method so we can output the result  
-
-componentDidMount() {
+//componentDidMount = () => {
+useEffect(() => {  // <----- 7atta law shelto ma bisir shi
   // and you can use axios.get also
   // see https://www.youtube.com/watch?v=IYCa1F-OWmk&ab_channel=TraversyMedia <--- (fetch + pagination)
+
+// I can use axios as well to fetch data from an API, the returned data is stringified 
+// in axios you don't need to use .json()  
 
   fetch('https://jsonplaceholder.typicode.com/users') // , (result) => result.json()) <-- totaly false
     .then((result) =>  result.json())  // result.json() to convert it to json format
       .then(json => {  // we want to take the json that we guess formated
-        this.setState({
-          items : json,
-          isLoaded : true
-        });
+        // this.setState({
+        //   items : json,
+        //   isLoaded : true
+        // });
+        updateItems(json);
+        setIsLoaded(true);
+        console.log ("i am in useEffect hook");
     })
     .catch((error) => {
         console.log("error");
     });
-};
+},[]);// [] means it only will runs when it mounts, which is what we want. 
+//},[count]);  this line means whenever the count variable changed, useEffect is called again.
 
-async AddData() {
+// current page
+const LastIndex = currentPage * itemperpage;
+const firstindex = LastIndex - itemperpage;
+const currentItems = items.slice(firstindex, LastIndex);
+
+const paginate = (number) => {
+  setCurrenPage(number);
+}
+
+
+const AddData = () => {
   try{ 
   fetch('URL', { 
      method:'POST',
@@ -66,45 +90,55 @@ async AddData() {
   }
 };
 
-render(){
-      var { items, isLoaded } = this.state;
-      if(!isLoaded)
-      {
-        return (<div>Please wait, data is loading ...</div>);
-      }
-      else
-      {
-            return (
-              <div className="container">
-                <div className="row">
-                  <div className="col-md-12">
-                    <Contacts />
-                  </div>
-                </div>
-                <div className="row">
-                  <div className="col-md-12">
-                    <User text="this is a text showing using props "/>
-                  </div>
-                </div>
-                <hr color="black"/> data fetched from API
-                <div>
-                  <button className="btn btn-primary" 
-                          type="submit" 
-                          onClick={ () => this.AddData }> Insert </button>
-                  <ul>
-                      { items.filter(u => u.id < 4).map(item => (
-                          <li key={ item.id }>
-                            Name:{ item.name } | Email:{ item.email }
-                          </li>)
-                        ) }
-                  </ul>
-                </div>
+//render(){
+      //var { items, isLoaded } = this.state;
+if(!isLoaded)
+  return (<div>Please wait, data is loading ...</div>);
 
-              </div>
-            );
-              }
-          }
-      }
+else
+  {
+    return (
+      <div className="container">
+        <div className="row">
+        <div className="col-md-12">
+          <Contacts />
+        </div>
+        </div>
+        <div className="row">
+        <div className="col-md-12">
+          <User text="this is a text showing using props "/>
+        </div>
+        </div>
+        <hr color="black"/> data fetched from API
+        <div>
+        <button className="btn btn-primary" 
+        type="submit" 
+        onClick={ () => this.AddData }> Insert </button>
+        <button className="btn btn-primary" 
+        type="submit" 
+        onClick={ () => increment(count + 1) }> increment </button>
+
+        <ul>
+        { currentItems.map(item => (
+        <li key={ item.id }>
+        Name:{ item.name } | Email:{ item.email }
+        </li>)
+        ) }
+        </ul>
+        <hr/>
+        <Pagination numberOfitems={ items.length } 
+                    itemperpage={ itemperpage } 
+                    paginate={ paginate }/>
+        </div>
+      </div>
+    );
+  }
+}
+export default App;
+
+
+          
+      
 /*
  JSX which allows you to write HTML tags inside the JavaScript code. Example 
  const JSXexample = (<table><tr><td>...</td></tr></table>);
@@ -122,5 +156,3 @@ hello = () => {
   return '...';
 }
  */
-
-export default App;
---
+++ src/Components/pagination.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Pagination = (props) => {
+
+const pages = [];
+for(let i =1; i <= Math.ceil(props.numberOfitems / props.itemperpage); i++)
+{
+  pages.push(i);
+}
+
+return(
+    <nav>
+    <ul className="pagination">
+        { pages.map(number =>( 
+            <li key={ number } className="page-item">   { /* key in li and not in <a>  */}
+                <a href="!#" className="page-link" onClick={ () => props.paginate(number) }>
+                    { number }
+                </a>
+            </li>
+        ))
+        }
+      </ul>
+    </nav>
+);
+
+
+}
+
+export default Pagination
